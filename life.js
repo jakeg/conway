1157809094c5f95588f974ea63e5ba4dbf4f1fb8
Jake Gordon <github@jakeg.co.uk>
2017-05-11 10:53:45 +0100

    tried with a simple array, 25ms

--- life.js
+++ life.js
@@ -13,10 +13,26 @@
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'white';
 
-  var cells = [
+
+  // x, y, alive, x, y, alive, ...
+  var cells = [];
+
+
+  for (var y=0; y<canvas.height; y++) {
+    for (var x=0; x<canvas.width; x++) {
+      //cells.push(0);
+      cells.push([x, y, 0]);
+    }
+  }
+
+  var starting_cells = [
     [0, 5],
     [50, 50],
   ];
+
+  starting_cells.forEach(function(cell) {
+    cells[cell[0] + (cell[1]-1) * canvas.width] = 1;
+  });
 
   draw();
 
@@ -26,15 +42,37 @@
     if (redraw) {
       console.log('drawing');
 
+      var start = Date.now();
+
       // blank the canvas
       ctx.fillStyle = 'black';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
       ctx.fillStyle = 'white';
 
-      
-      
+      /*
+      var i=0;
+      for (var y=0; y<canvas.height; y++) {
+        for (var x=0; x<canvas.width; x++) {
+          if (cells[i]) {
+            ctx.fillRect(x, y, 1, 1);
+            cells[i] = 0;
+            cells[i-1] = 1;
+          }
+          i++;
+        }
+      }
+      */
 
+      cells.forEach(function(cell) {
+        if (cell[2]) ctx.fillRect(cell[0], cell[1], 1, 1);
+        cell[0]++;
+        cell[1]++;
+        if (cell[0] > canvas.width) cell[0] = 0;
+        if (cell[1] > canvas.height) cell[1] = 0;
+      });
+
+      /*
       cells.forEach(function(cell) {
         ctx.fillRect(cell[0], cell[1], 1, 1);
         cell[0]++;
@@ -42,8 +80,10 @@
         if (cell[0] > canvas.width) cell[0] = 0;
         if (cell[1] > canvas.height) cell[1] = 0;
       });
+      */
 
-      // redraw = false;
+      var end = Date.now();
+      ctx.fillText(end - start + 'ms', 10, canvas.height - 20);
     }
     window.requestAnimationFrame(draw);
   }
@@ -64,5 +104,6 @@
 
   window.pause = pause;
   window.unpause = unpause;
+  window.cells = cells;
 
 })();
