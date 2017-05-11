d2386c0d3ac7aa050ef6697ab8295d7854e66bb6
Jake Gordon <github@jakeg.co.uk>
2017-05-11 13:08:24 +0100

    ready to put live

--- life.js
+++ life.js
@@ -5,19 +5,23 @@
   var ctx = canvas.getContext('2d');
   var starting_cells = [];
   var redraw = false;
+  var last_start = 0;
+  var speed = 15; // speed/1000 is the max frames per second we try to draw. so speed = 10 gives 100fps max
   var zoom = 8;
   var width;
   var height;
 
   window.addEventListener('resize', resizeCanvas);
   window.addEventListener('keydown', function(e) {
-    if (e.keyCode == 32) redraw = !redraw;
+    console.log(e.keyCode);
+    if (e.keyCode == 32) redraw = !redraw; // space bar
+    if (e.keyCode == 187) speed = Math.min(100, speed + 1);
+    if (e.keyCode == 189) speed = Math.max(1, speed - 1);
   });
   canvas.addEventListener('click', function(e) {
     // toggle the cell we've clicked
     var x = Math.floor(e.offsetX/zoom);
     var y = Math.floor(e.offsetY/zoom);
-    console.log(e.offsetX, x);
     cells[x][y] = !cells[x][y];
     ctx.fillStyle = cells[x][y] ? 'white' : 'black';
     if (!redraw) {
@@ -44,13 +48,25 @@
     });
   }
 
+  var starting_cells = [
+    [30, 60],
+    [30, 61],
+    [30, 62],
+    [31, 60],
+    [29, 61]
+  ];
+
+  starting_cells.forEach(function(cell) {
+    cells[cell[0]][cell[1]] = true;
+  });
+
   draw();
   loop();
 
   // FUNCTIONS
 
   function loop() {
-    if (redraw) draw();
+    if (redraw && Date.now() - last_start > 1000/speed) draw();
     window.requestAnimationFrame(loop);
   }
 
@@ -58,6 +74,7 @@
     console.log('drawing');
 
     var start = Date.now();
+    last_start = start;
 
     // blank the canvas
     ctx.fillStyle = 'black';
@@ -108,7 +125,14 @@
 
 
     var end = Date.now();
-    ctx.fillText(width + ' x ' + height + ' ~ ' + (end - start) + 'ms ~ ' + Math.round(1000 / (end - start)) + 'fps', 10, canvas.height - 20);
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.fillRect(5, 5, 150, 70);
+    ctx.fillStyle = '#fff';
+    ctx.font = '10px sans-serif';
+    ctx.fillText(width + ' x ' + height + ' ~ ' + (end - start) + 'ms ~ ' + Math.round(1000 / (end - start)) + 'fps', 10, 20);
+    ctx.font = '20px sans-serif';
+    ctx.fillText('Speed: ' + speed + 'x', 10, 45);
   }
 
   function pause() {
